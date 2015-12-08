74965646259d6a91f35f32c13d6cbc155075c34c
camihayes <camihayes72@gmail.com>
2015-12-08 18:21:10 +0000

    complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
 
   def new
@@ -53,6 +55,13 @@ class ArticlesController < ApplicationController
     end
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles."
+      redirect_to root_path
+    end
   end
 
 end
